ac20ac54e9e6867abc6681fee1b077f8967132fc
omer omer <omer9345@gmail.com>
2019-04-04 09:46:55 +0200

    modify test lcd

--- test_lcd/test_lcd.ino
+++ test_lcd/test_lcd.ino
@@ -1,40 +1,64 @@
-#include <dht_nonblocking.h>
-
-
 /*
-  Affichage de la température et de l'humidité ambiante sur un écran LCD.
-  Author : Alexandre PERETJATKO.
+  LiquidCrystal Library - Hello World
+
+ Demonstrates the use a 16x2 LCD display.  The LiquidCrystal
+ library works with all LCD displays that are compatible with the
+ Hitachi HD44780 driver. There are many of them out there, and you
+ can usually tell them by the 16-pin interface.
+
+ This sketch prints "Hello World!" to the LCD
+ and shows the time.
+
+  The circuit:
+ * LCD RS pin to digital pin 12
+ * LCD Enable pin to digital pin 11
+ * LCD D4 pin to digital pin 5
+ * LCD D5 pin to digital pin 4
+ * LCD D6 pin to digital pin 3
+ * LCD D7 pin to digital pin 2
+ * LCD R/W pin to ground
+ * LCD VSS pin to ground
+ * LCD VCC pin to 5V
+ * 10K resistor:
+ * ends to +5V and ground
+ * wiper to LCD VO pin (pin 3)
+
+ Library originally added 18 Apr 2008
+ by David A. Mellis
+ library modified 5 Jul 2009
+ by Limor Fried (http://www.ladyada.net)
+ example added 9 Jul 2009
+ by Tom Igoe
+ modified 22 Nov 2010
+ by Tom Igoe
+ modified 7 Nov 2016
+ by Arturo Guadalupi
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/LiquidCrystalHelloWorld
+
 */
-// ------------------------------------------------------------------
-// Ecran LCD
+
+// include the library code:
 #include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-// ------------------------------------------------------------------
-// for DHT11, ( Capteur Temperature + Humidité )
-//      VCC: 5V or 3V
-//      GND: GND
-//      DATA: pinDHT11
 
-long a;
-// =======================================================================
 void setup() {
-  Serial.begin(9600);
-  // Initialisation de l'écran LCD
-  lcd.begin(16, 2); // 16 colonnes et 2 lignes
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("hello, world!");
 }
-// =======================================================================
+
 void loop() {
-  // Affichage sur le terminal
-  Serial.println("=================================");
-  // Lecture de la température et de l'humidité ------------------
-a=sr04.Distance();
-
-  }
-
-  // Ecran LCD --------------------------------------------------
-  lcd.setCursor(0, 0);
-  lcd.print("Temperature: "); lcd.print((long)a); lcd.print("C");
- // lcd.setCursor(0, 1);
- // lcd.print("Humidite   : ");    lcd.print((int)humidity);    lcd.print("%");
-  delay(2000);
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis() / 1000);
+}

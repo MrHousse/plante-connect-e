716403d4ea618533c48f479278a776ce459ab41e
omer omer <omer9345@gmail.com>
2019-04-11 11:47:15 +0200

    finished implantation off humidithy and temperature with lcd

--- test_lcd/test_lcd.ino
+++ test_lcd/test_lcd.ino
@@ -1,10 +1,15 @@
 #include <LiquidCrystal.h>
+#include <dht.h>
 
+dht DHT;
+
+#define DHT11_PIN 7
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 const int plante = A0;
+const int led = 8;
 
 void setup() {
   // set up the LCD's number of columns and rows:
@@ -13,15 +18,39 @@ void setup() {
   //lcd.print("hello, world!");
   // Capteur d'hygrométrie
   pinMode(plante, INPUT);
+  pinMode(led, OUTPUT);
 }
 
 void loop() {
   int humide = analogRead(plante);
-  // Turn off the display:
-  String humdv = "humidée : ";
-  String humd = humdv + humide ;
-  lcd.print(humd);
-  delay(500);
+  int chk = DHT.read11(DHT11_PIN);
+  int temper = DHT.temperature;
+  // converti les varriable int en string
+  String humdv = "humidite : ";
+  if (humide <= 50 ){
+    String humd = "Arrosez";
+    lcd.setCursor(0, 0);
+    lcd.print(humd);
+  }
+  else {
+    String humd = humdv + humide ;
+    lcd.setCursor(0, 0);
+    lcd.print(humd);
+  }
+
+  if (temper >= 28) {
+    //ouvrir serre
+    digitalWrite(led, HIGH);
+  }
+  else{
+    //rien faire
+  }
+  String tempv = "Temperature : ";
+  String temp = tempv + temper ;
+
+  lcd.setCursor(0, 1);
+  lcd.print(temp);
+  delay(2000);
   // Turn on the display:
   lcd.clear();
  
